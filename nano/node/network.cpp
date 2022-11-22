648f0679e27ea18e365c3e14122b490ac75b2df2
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2022-11-23 00:29:25 +0100

    Logging: flood vote hashes

--- nano/node/network.cpp
+++ nano/node/network.cpp
@@ -225,7 +225,7 @@ void nano::network::flood_vote (std::shared_ptr<nano::vote> const & vote_a, floa
 	nano::confirm_ack message{ node.network_params.network, vote_a };
 	for (auto & i : list (fanout (scale)))
 	{
-		node.logger.always_log (boost::format ("FLOOD VOTE: %1%") % i->get_endpoint ());
+		node.logger.always_log (boost::format ("FLOOD VOTE: %1% [%2%]") % i->get_endpoint () % vote_a->hashes_string ());
 
 		i->send (message, nullptr);
 	}
@@ -236,7 +236,7 @@ void nano::network::flood_vote_pr (std::shared_ptr<nano::vote> const & vote_a)
 	nano::confirm_ack message{ node.network_params.network, vote_a };
 	for (auto const & i : node.rep_crawler.principal_representatives ())
 	{
-		node.logger.always_log (boost::format ("FLOOD VOTE PR: %1% : %2%") % i.account.to_account () % i.channel->get_endpoint ());
+		node.logger.always_log (boost::format ("FLOOD VOTE PR: %1% : %2% [%3%]") % i.account.to_account () % i.channel->get_endpoint () % vote_a->hashes_string ());
 
 		i.channel->send (message, nullptr, nano::buffer_drop_policy::no_limiter_drop);
 	}
