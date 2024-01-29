6158aa4662fcadbf196133f08ba33b4d443cece9
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2024-01-30 00:30:42 +0100

    JSON FIXES

--- nano/lib/object_stream.cpp
+++ nano/lib/object_stream.cpp
@@ -11,15 +11,17 @@ nano::object_stream_config const & nano::object_stream_config::json_config ()
 	static object_stream_config const config{
 		.field_name_begin = "\"",
 		.field_name_end = "\"",
-		.field_assignment = ": ",
-		.field_separator = ", ",
+		.field_assignment = ":",
+		.field_separator = ",",
 		.object_begin = "{",
 		.object_end = "}",
 		.array_begin = "[",
 		.array_end = "]",
 		.array_element_begin = "",
 		.array_element_end = "",
-		.array_element_separator = ", ",
+		.array_element_separator = ",",
+		.indent = "",
+		.newline = "",
 		.precision = 4,
 	};
 	return config;
