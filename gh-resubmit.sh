1806745bd24b80d0d1b3f0749cb28d2f4a7cccb3
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2024-02-12 09:59:22 +0100

    RESUBMIT SCRIPT

--- gh-resubmit.sh
+++ gh-resubmit.sh
@@ -6,13 +6,15 @@ times=5
 # Current branch
 branch=$(git rev-parse --abbrev-ref HEAD)
 
-for i in $(seq 1 $times); do
+GIT_COMMITTER_DATE="$(date)" git commit --amend --no-edit --date "$(date)"
+
+#for i in $(seq 1 $times); do
     # Change the commit date and keep the commit message the same
-    GIT_COMMITTER_DATE="$(date)" git commit --amend --no-edit --date "$(date)"
+
 
     # Force push the current branch
-    git push --force
+#    git push --force
 
     # Wait a bit to avoid any potential rate limiting issues (optional)
-    sleep 5
-done
+#    sleep 5
+#done
