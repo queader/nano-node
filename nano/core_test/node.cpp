48591d7062c749bf331472564157d146f308a00e
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2024-03-22 22:09:02 +0100

    Fix tests

--- nano/core_test/node.cpp
+++ nano/core_test/node.cpp
@@ -38,6 +38,7 @@ TEST (node, null_account)
 TEST (node, stop)
 {
 	nano::test::system system (1);
+	system.io_guard.reset ();
 	ASSERT_NE (system.nodes[0]->wallets.items.end (), system.nodes[0]->wallets.items.begin ());
 	system.nodes[0]->stop ();
 	system.io_ctx->run ();
