cef739408af7d54c88c74f116d4a4c1d8e0e7286
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2024-04-04 23:35:28 +0200

    INTRODUCE MESSAGE PROCESSOR

--- nano/node/nodeconfig.hpp
+++ nano/node/nodeconfig.hpp
@@ -12,6 +12,7 @@
 #include <nano/node/bootstrap/bootstrap_config.hpp>
 #include <nano/node/bootstrap/bootstrap_server.hpp>
 #include <nano/node/ipc/ipc_config.hpp>
+#include <nano/node/message_processor.hpp>
 #include <nano/node/peer_history.hpp>
 #include <nano/node/repcrawler.hpp>
 #include <nano/node/scheduler/hinted.hpp>
@@ -39,6 +40,8 @@ enum class frontiers_confirmation_mode : uint8_t
 	invalid
 };
 
+class message_processor_config;
+
 /**
  * Node configuration
  */
@@ -48,6 +51,7 @@ public:
 	// TODO: Users of this class rely on the default copy consturctor. This prevents using unique_ptrs with forward declared types.
 	node_config (nano::network_params & network_params = nano::dev::network_params);
 	node_config (const std::optional<uint16_t> &, nano::network_params & network_params = nano::dev::network_params);
+	~node_config ();
 
 	nano::error serialize_toml (nano::tomlconfig &) const;
 	nano::error deserialize_toml (nano::tomlconfig &);
@@ -147,6 +151,7 @@ public:
 	nano::vote_processor_config vote_processor;
 	nano::peer_history_config peer_history;
 	nano::transport::tcp_config tcp;
+	nano::message_processor_config message_processor;
 
 public:
 	std::string serialize_frontiers_confirmation (nano::frontiers_confirmation_mode) const;
