0a44a28c14761429f9010e15ce6f4abd31eae56d
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2024-05-16 11:49:40 +0200

    WIP

--- nano/node/active_elections.hpp
+++ nano/node/active_elections.hpp
@@ -3,19 +3,14 @@
 #include <nano/lib/enum_util.hpp>
 #include <nano/lib/numbers.hpp>
 #include <nano/node/election_behavior.hpp>
-#include <nano/node/election_insertion_result.hpp>
+#include <nano/node/election_container.hpp>
 #include <nano/node/election_status.hpp>
+#include <nano/node/fwd.hpp>
 #include <nano/node/recently_cemented_cache.hpp>
 #include <nano/node/recently_confirmed_cache.hpp>
 #include <nano/node/vote_router.hpp>
 #include <nano/node/vote_with_weight_info.hpp>
 #include <nano/secure/common.hpp>
-
-#include <boost/multi_index/hashed_index.hpp>
-#include <boost/multi_index/member.hpp>
-#include <boost/multi_index/random_access_index.hpp>
-#include <boost/multi_index/sequenced_index.hpp>
-#include <boost/multi_index_container.hpp>
 
 #include <condition_variable>
 #include <deque>
@@ -23,23 +18,9 @@
 #include <thread>
 #include <unordered_map>
 
-namespace mi = boost::multi_index;
-
 namespace nano
 {
-class node;
-class active_elections;
-class block;
-class block_sideband;
-class block_processor;
-class confirming_set;
-class election;
-class vote;
-class stats;
-}
-namespace nano::secure
-{
-class read_transaction;
+enum class election_state;
 }
 
 namespace nano
@@ -71,36 +52,7 @@ public:
  */
 class active_elections final
 {
-public:
-	using erased_callback_t = std::function<void (std::shared_ptr<nano::election>)>;
-
-private: // Elections
-	class entry final
-	{
-	public:
-		nano::qualified_root root;
-		std::shared_ptr<nano::election> election;
-		erased_callback_t erased_callback;
-	};
-
 	friend class nano::election;
-
-	// clang-format off
-	class tag_account {};
-	class tag_root {};
-	class tag_sequenced {};
-	class tag_uncemented {};
-	class tag_arrival {};
-	class tag_hash {};
-
-	using ordered_roots = boost::multi_index_container<entry,
-	mi::indexed_by<
-		mi::sequenced<mi::tag<tag_sequenced>>,
-		mi::hashed_unique<mi::tag<tag_root>,
-			mi::member<entry, nano::qualified_root, &entry::root>>
-	>>;
-	// clang-format on
-	ordered_roots roots;
 
 public:
 	active_elections (nano::node &, nano::confirming_set &, nano::block_processor &);
@@ -109,49 +61,69 @@ public:
 	void start ();
 	void stop ();
 
-	/**
-	 * Starts new election with a specified behavior type
-	 */
-	nano::election_insertion_result insert (std::shared_ptr<nano::block> const &, nano::election_behavior = nano::election_behavior::priority, erased_callback_t = nullptr);
+	struct insert_result
+	{
+		std::shared_ptr<nano::election> election{ nullptr };
+		bool inserted{ false };
+	};
+
+	insert_result insert (
+	std::shared_ptr<nano::block> const &,
+	nano::election_behavior = nano::election_behavior::priority,
+	nano::bucket_t bucket = 0,
+	nano::priority_t priority = 0);
+
+	// Notify this container about a new block (potential fork)
+	bool publish (std::shared_ptr<nano::block> const &);
+
 	// Is the root of this block in the roots container
 	bool active (nano::block const &) const;
 	bool active (nano::qualified_root const &) const;
 	std::shared_ptr<nano::election> election (nano::qualified_root const &) const;
+
 	// Returns a list of elections sorted by difficulty
 	std::vector<std::shared_ptr<nano::election>> list_active (std::size_t = std::numeric_limits<std::size_t>::max ());
+
 	bool erase (nano::block const &);
 	bool erase (nano::qualified_root const &);
+
 	bool empty () const;
-	std::size_t size () const;
-	bool publish (std::shared_ptr<nano::block> const &);
-	void block_cemented_callback (std::shared_ptr<nano::block> const &);
-	void block_already_cemented_callback (nano::block_hash const &);
+	size_t size () const;
+	size_t size (nano::election_behavior) const;
+	size_t size (nano::election_behavior, nano::bucket_t) const;
 
-	/**
-	 * Maximum number of elections that should be present in this container
-	 * NOTE: This is only a soft limit, it is possible for this container to exceed this count
-	 */
+	// Returns election with the largerst "priority" number (highest timestamp). NOTE: Lower "priority" is better.
+	using top_entry_t = std::pair<std::shared_ptr<nano::election>, priority_t>;
+	top_entry_t top (nano::election_behavior, nano::bucket_t) const;
+
+	// Maximum number of elections that should be present in this container
+	// NOTE: This is only a soft limit, it is possible for this container to exceed this count
 	int64_t limit (nano::election_behavior behavior) const;
-	/**
-	 * How many election slots are available for specified election type
-	 */
+	// How many election slots are available for specified election type
 	int64_t vacancy (nano::election_behavior behavior) const;
-	std::function<void ()> vacancy_update{ [] () {} };
 
+public: // TODO: Move to a separate class
 	std::size_t election_winner_details_size ();
 	void add_election_winner_details (nano::block_hash const &, std::shared_ptr<nano::election> const &);
 	std::shared_ptr<nano::election> remove_election_winner_details (nano::block_hash const &);
+
+public: // Events
+	// TODO: Use observer_set
+	std::function<void ()> vacancy_update{ [] () {} };
 
 private:
 	void request_loop ();
 	void request_confirm (nano::unique_lock<nano::mutex> &);
 	// Erase all blocks from active and, if not confirmed, clear digests from network filters
-	void cleanup_election (nano::unique_lock<nano::mutex> & lock_a, std::shared_ptr<nano::election>);
-	nano::stat::type completion_type (nano::election const & election) const;
+	void erase_impl (nano::unique_lock<nano::mutex> &, std::shared_ptr<nano::election>);
 	// Returns a list of elections sorted by difficulty, mutex must be locked
 	std::vector<std::shared_ptr<nano::election>> list_active_impl (std::size_t) const;
 	void activate_successors (nano::secure::read_transaction const & transaction, std::shared_ptr<nano::block> const & block);
 	void notify_observers (nano::secure::read_transaction const & transaction, nano::election_status const & status, std::vector<nano::vote_with_weight_info> const & votes);
+	void block_cemented_callback (std::shared_ptr<nano::block> const &);
+	void block_already_cemented_callback (nano::block_hash const &);
+
+	static nano::stat::type to_completion_type (nano::election_state);
 
 private: // Dependencies
 	active_elections_config const & config;
@@ -160,28 +132,26 @@ private: // Dependencies
 	nano::block_processor & block_processor;
 
 public:
-	recently_confirmed_cache recently_confirmed;
-	recently_cemented_cache recently_cemented;
+	nano::recently_confirmed_cache recently_confirmed;
+	nano::recently_cemented_cache recently_cemented;
 
 	// TODO: This mutex is currently public because many tests access it
 	// TODO: This is bad. Remove the need to explicitly lock this from any code outside of this class
 	mutable nano::mutex mutex{ mutex_identifier (mutexes::active) };
 
 private:
+	nano::election_container elections;
+
 	nano::mutex election_winner_details_mutex{ mutex_identifier (mutexes::election_winner_details) };
 	std::unordered_map<nano::block_hash, std::shared_ptr<nano::election>> election_winner_details;
 
 	// Maximum time an election can be kept active if it is extending the container
 	std::chrono::seconds const election_time_to_live;
 
-	/** Keeps track of number of elections by election behavior (normal, hinted, optimistic) */
-	nano::enum_array<nano::election_behavior, int64_t> count_by_behavior;
-
 	nano::condition_variable condition;
 	bool stopped{ false };
 	std::thread thread;
 
-	friend class election;
 	friend std::unique_ptr<container_info_component> collect_container_info (active_elections &, std::string const &);
 
 public: // Tests
---
+++ nano/node/election_container.cpp
@@ -0,0 +1,120 @@
+#include <nano/node/election.hpp>
+#include <nano/node/election_container.hpp>
+
+#include <ranges>
+
+void nano::election_container::insert (std::shared_ptr<nano::election> const & election, nano::election_behavior behavior, nano::bucket_t bucket, nano::priority_t priority)
+{
+	debug_assert (election->behavior () == behavior);
+	debug_assert (!exists (election)); // Should be checked before inserting
+	debug_assert (!entries.get<tag_ptr> ().contains (election));
+	debug_assert (!entries.get<tag_root> ().contains (election->qualified_root));
+
+	auto [it, inserted] = entries.emplace_back (entry{ election, election->qualified_root, behavior, bucket, priority });
+	debug_assert (inserted);
+
+	// Update cached size
+	size_by_behavior[{ behavior }]++;
+	size_by_bucket[{ behavior, bucket }]++;
+}
+
+bool nano::election_container::erase (std::shared_ptr<nano::election> const & election)
+{
+	auto maybe_entry = info (election);
+	if (!maybe_entry)
+	{
+		return false; // Not found
+	}
+	auto entry = *maybe_entry;
+
+	auto erased = entries.get<tag_ptr> ().erase (election);
+	release_assert (erased == 1);
+
+	// Update cached size
+	size_by_behavior[{ entry.behavior }]--;
+	size_by_bucket[{ entry.behavior, entry.bucket }]--;
+
+	return true; // Erased
+}
+
+bool nano::election_container::exists (nano::qualified_root const & root) const
+{
+	return entries.get<tag_root> ().contains (root);
+}
+
+bool nano::election_container::exists (std::shared_ptr<nano::election> const & election) const
+{
+	return entries.get<tag_ptr> ().contains (election);
+}
+
+std::shared_ptr<nano::election> nano::election_container::election (nano::qualified_root const & root) const
+{
+	if (auto existing = entries.get<tag_root> ().find (root); existing != entries.get<tag_root> ().end ())
+	{
+		return existing->election;
+	}
+	return nullptr;
+}
+
+auto nano::election_container::info (std::shared_ptr<nano::election> const & election) const -> std::optional<entry>
+{
+	if (auto existing = entries.get<tag_ptr> ().find (election); existing != entries.get<tag_ptr> ().end ())
+	{
+		return *existing;
+	}
+	return std::nullopt;
+}
+
+auto nano::election_container::list () const -> std::vector<entry>
+{
+	auto r = entries.get<tag_sequenced> () | std::views::transform ([] (auto const & entry) { return entry; });
+	return { r.begin (), r.end () };
+}
+
+size_t nano::election_container::size () const
+{
+	return entries.size ();
+}
+
+size_t nano::election_container::size (nano::election_behavior behavior) const
+{
+	if (auto existing = size_by_behavior.find ({ behavior }); existing != size_by_behavior.end ())
+	{
+		return existing->second;
+	}
+	return 0;
+}
+
+size_t nano::election_container::size (nano::election_behavior behavior, nano::bucket_t bucket) const
+{
+	if (auto existing = size_by_bucket.find ({ behavior, bucket }); existing != size_by_bucket.end ())
+	{
+		return existing->second;
+	}
+	return 0;
+}
+
+auto nano::election_container::top (nano::election_behavior behavior, nano::bucket_t bucket) const -> top_entry_t
+{
+	auto & index = entries.get<tag_key> ();
+
+	// Returns an iterator pointing to the first element with key greater than x
+	auto existing = index.upper_bound (key{ behavior, bucket, std::numeric_limits<nano::priority_t>::max () });
+	if (existing != index.begin ())
+	{
+		--existing;
+		if (existing->behavior == behavior && existing->bucket == bucket)
+		{
+			return { existing->election, existing->priority };
+		}
+	}
+
+	return { nullptr, std::numeric_limits<nano::priority_t>::max () };
+}
+
+void nano::election_container::clear ()
+{
+	entries.clear ();
+	size_by_behavior.clear ();
+	size_by_bucket.clear ();
+}
