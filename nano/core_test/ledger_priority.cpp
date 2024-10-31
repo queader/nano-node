fad24c3f6624f9aa83611c4c8b97abad7cebb563
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2024-10-31 12:56:54 +0100

    Test ledger priority

---
+++ nano/core_test/ledger_priority.cpp
@@ -0,0 +1,8 @@
+#include <nano/secure/ledger.hpp>
+#include <nano/secure/ledger_set_any.hpp>
+#include <nano/secure/ledger_set_confirmed.hpp>
+#include <nano/test_common/ledger_context.hpp>
+#include <nano/test_common/make_store.hpp>
+#include <nano/test_common/testutil.hpp>
+
+#include <gtest/gtest.h>
