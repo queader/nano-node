597f27d72f5a6b68dabbbf9c4c0beb9572de56df
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2024-11-02 10:03:48 +0100

    Time utils

--- nano/lib/utility.hpp
+++ nano/lib/utility.hpp
@@ -165,6 +165,54 @@ bool elapse (nano::clock::time_point & last, Duration const & duration)
 	}
 	return false;
 }
+
+template <class Clock>
+auto microseconds (std::chrono::time_point<Clock> time)
+{
+	return std::chrono::duration_cast<std::chrono::microseconds> (time.time_since_epoch ()).count ();
+}
+
+template <class Duration>
+auto microseconds (Duration duration)
+{
+	return std::chrono::duration_cast<std::chrono::microseconds> (duration).count ();
+}
+
+template <class Clock>
+auto milliseconds (std::chrono::time_point<Clock> time)
+{
+	return std::chrono::duration_cast<std::chrono::milliseconds> (time.time_since_epoch ()).count ();
+}
+
+template <class Duration>
+auto milliseconds (Duration duration)
+{
+	return std::chrono::duration_cast<std::chrono::milliseconds> (duration).count ();
+}
+
+template <class Clock>
+auto seconds (std::chrono::time_point<Clock> time)
+{
+	return std::chrono::duration_cast<std::chrono::seconds> (time.time_since_epoch ()).count ();
+}
+
+template <class Duration>
+auto seconds (Duration duration)
+{
+	return std::chrono::duration_cast<std::chrono::seconds> (duration).count ();
+}
+
+template <class Clock>
+auto milliseconds_delta (std::chrono::time_point<Clock> time, std::chrono::time_point<Clock> now = Clock::now ())
+{
+	return std::chrono::duration_cast<std::chrono::milliseconds> (now - time).count ();
+}
+
+template <class Clock>
+auto seconds_delta (std::chrono::time_point<Clock> time, std::chrono::time_point<Clock> now = Clock::now ())
+{
+	return std::chrono::duration_cast<std::chrono::seconds> (now - time).count ();
+}
 }
 
 namespace nano::util
