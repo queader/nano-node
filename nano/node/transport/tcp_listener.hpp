32d26cecc2d22faa960f7fc40b980473e057cace
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2024-11-14 20:43:59 +0100

    Keep connection type

--- nano/node/transport/tcp_listener.hpp
+++ nano/node/transport/tcp_listener.hpp
@@ -132,6 +132,7 @@ private:
 private:
 	struct connection
 	{
+		connection_type type;
 		asio::ip::tcp::endpoint endpoint;
 		std::weak_ptr<nano::transport::tcp_socket> socket;
 		std::weak_ptr<nano::transport::tcp_server> server;
