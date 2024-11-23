1c6280da668b616002819d34524a2143f0689f01
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2024-11-23 16:39:29 +0100

    Fwd

--- nano/lib/fwd.hpp
+++ nano/lib/fwd.hpp
@@ -4,13 +4,12 @@
 #include <iosfwd>
 
 struct uint8_char_traits;
+
 namespace nano
 {
 class block;
-enum class block_type : uint8_t;
 class block_visitor;
 class container_info;
-enum class epoch : uint8_t;
 class jsonconfig;
 class mutable_block_visitor;
 class network_constants;
@@ -19,6 +18,9 @@ class thread_pool;
 class tomlconfig;
 template <typename Key, typename Value>
 class uniquer;
+
+enum class block_type : uint8_t;
+enum class epoch : uint8_t;
 enum class work_version;
 
 using stream = std::basic_streambuf<uint8_t, uint8_char_traits>;
@@ -26,6 +28,7 @@ using stream = std::basic_streambuf<uint8_t, uint8_char_traits>;
 
 namespace nano::stat
 {
+enum class type;
 enum class detail;
 enum class dir;
 }
